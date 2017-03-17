597120df575f5cd6117a2797569d44b9b6336527
ohemeng <francis_adu@yahoo.com>
2017-03-16 23:34:30 -0400

    d09 exercise

---
+++ d09/materials/travelling-dwarf/solution.js
@@ -0,0 +1,62 @@
+console.log("WRITE YOUR SOLUTION IN HERE!");
+x = 0;
+
+while (x < 4) {
+	d.move();
+	x = x + 1;
+}
+
+d.orient("down");
+
+x = 0;
+
+while (x < 16) {
+	d.move();
+	x = x + 1;
+}
+
+d.orient("right");
+
+x = 0;
+
+while (x < 6	) {
+	d.move();
+	x = x + 1;
+}
+
+d.orient("up");
+
+x = 0;
+
+while (x < 11) {
+	d.move();
+	x = x + 1;
+}
+
+d.orient("right");
+
+x = 0;
+
+while (x < 6) {
+	d.move();
+	x = x + 1;
+}
+
+d.orient("down");
+
+x = 0;
+
+while (x < 9) {
+	d.move();
+	x = x + 1;
+}
+
+d.orient("right");
+
+x = 0;
+
+
+while (x < 2) {
+	d.move();
+	x = x + 1;
+}
